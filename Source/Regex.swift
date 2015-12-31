59448a46a00ec22ffb4ce7b8f4b675ed3011bcf9
Adam Sharp <adsharp@me.com>
2015-12-31 15:31:16 +1100

    Add `Regex.lastMatch` property for use during pattern matching

--- Source/Regex.swift
+++ Source/Regex.swift
@@ -37,6 +37,9 @@ public struct Regex: StringLiteralConvertible, CustomStringConvertible, CustomDe
   // MARK: Matching
 
   /// Returns `true` if the regex matches `string`, otherwise returns `false`.
+  ///
+  /// - note: If the match is successful, `Regex.lastMatch` will be set with the
+  ///   result of the match.
   public func matches(string: String) -> Bool {
     return match(string) != nil
   }
@@ -48,8 +51,12 @@ public struct Regex: StringLiteralConvertible, CustomStringConvertible, CustomDe
   /// - parameter string: The string to match against.
   ///
   /// - returns: An optional `MatchResult` describing the first match, or `nil`.
+  ///
+  /// - note: If the match is successful, the result is also stored in `Regex.lastMatch`.
   public func match(string: String) -> MatchResult? {
-    return regex.firstMatchInString(string, options: [], range: string.entireRange).map { MatchResult(string.utf16, $0) }
+    let match = regex.firstMatchInString(string, options: [], range: string.entireRange).map { MatchResult(string.utf16, $0) }
+    Regex._lastMatch = match
+    return match
   }
 
   /// If the regex matches `string`, returns an array of `MatchResult`, describing
@@ -59,8 +66,37 @@ public struct Regex: StringLiteralConvertible, CustomStringConvertible, CustomDe
   /// - parameter string: The string to match against.
   ///
   /// - returns: An array of `MatchResult` describing every match in `string`.
+  ///
+  /// - note: If there is at least one match, the first is stored in `Regex.lastMatch`.
   public func allMatches(string: String) -> [MatchResult] {
-    return regex.matchesInString(string, options: [], range: string.entireRange).map { MatchResult(string.utf16, $0) }
+    let matches = regex.matchesInString(string, options: [], range: string.entireRange).map { MatchResult(string.utf16, $0) }
+    if let firstMatch = matches.first { Regex._lastMatch = firstMatch }
+    return matches
+  }
+
+  // MARK: Accessing the last match
+
+  /// After any match, the result will be stored in this property for later use.
+  /// This is useful when pattern matching:
+  ///
+  ///     switch "hello" {
+  ///     case Regex("l+"):
+  ///       let count = Regex.lastMatch!.matchedString.characters.count
+  ///       print("matched \(count) characters")
+  ///     default:
+  ///       break
+  ///     }
+  ///
+  /// This property uses thread-local storage, and thus is thread safe.
+  public static var lastMatch: MatchResult? {
+    return _lastMatch
+  }
+
+  private static let _lastMatchKey = "me.sharplet.Regex.lastMatch"
+
+  private static var _lastMatch: MatchResult? {
+    get { return ThreadLocal(_lastMatchKey).value }
+    set { ThreadLocal(_lastMatchKey).value = newValue }
   }
 
   // MARK: Describing
