0936607bb6cfc87c0988fe0e12557d8385eb2529
Adam Sharp <adsharp@me.com>
2016-01-30 22:18:23 +1100

    Document string replacement API

--- Source/String+ReplaceMatching.swift
+++ Source/String+ReplaceMatching.swift
@@ -2,6 +2,18 @@ extension String {
 
   // MARK: Replacing the first match
 
+  /// If `regex` matches at least one substring, replace the first match with
+  /// `template`.
+  ///
+  /// The template string may be a literal string, or include template variables:
+  /// the variable `$0` will be replaced with the entire matched substring, `$1`
+  /// with the first capture group, etc.
+  ///
+  /// - note: To include the literal string "$1" in the replacement string,
+  ///   you must escape the "$": `\$1`.
+  ///
+  /// - parameter regex: A regular expression to match against `self`.
+  /// - parameter template: A template string used to replace matches.
   public mutating func replaceFirstMatching(regex: Regex, with template: String) {
     if let match = regex.match(self) {
       let replacement = regex
@@ -15,22 +27,79 @@ extension String {
     }
   }
 
+  /// If the regular expression described by `pattern` matches at least one
+  /// substring, replace the first match with `template`.
+  ///
+  /// The template string may be a literal string, or include template variables:
+  /// the variable `$0` will be replaced with the entire matched substring, `$1`
+  /// with the first capture group, etc.
+  ///
+  /// - note: To include the literal string "$1" in the replacement string,
+  ///   you must escape the "$": `\$1`.
+  ///
+  /// - requires: `pattern` is a valid regular expression. Invalid regular
+  ///   expressions will cause this method to trap.
+  ///
+  /// - parameter pattern: A regular expression pattern to match against `self`.
+  /// - parameter template: A template string used to replace matches.
   public mutating func replaceFirstMatching(pattern: String, with template: String) {
     replaceFirstMatching(Regex(pattern), with: template)
   }
 
+  /// Returns a new string where the first match of `regex` is replaced with
+  /// `template`.
+  ///
+  /// The template string may be a literal string, or include template variables:
+  /// the variable `$0` will be replaced with the entire matched substring, `$1`
+  /// with the first capture group, etc.
+  ///
+  /// - note: To include the literal string "$1" in the replacement string,
+  ///   you must escape the "$": `\$1`.
+  ///
+  /// - parameter regex: A regular expression to match against `self`.
+  /// - parameter template: A template string used to replace matches.
+  ///
+  /// - returns: A string with the first match of `regex` replaced by `template`.
   public func replacingFirstMatching(regex: Regex, with template: String) -> String {
     var string = self
     string.replaceFirstMatching(regex, with: template)
     return string
   }
 
+  /// Returns a new string where the first match of the regular expression
+  /// described by `pattern` is replaced with `template`.
+  ///
+  /// The template string may be a literal string, or include template variables:
+  /// the variable `$0` will be replaced with the entire matched substring, `$1`
+  /// with the first capture group, etc.
+  ///
+  /// - note: To include the literal string "$1" in the replacement string,
+  ///   you must escape the "$": `\$1`.
+  ///
+  /// - requires: `pattern` is a valid regular expression. Invalid regular
+  ///   expressions will cause this method to trap.
+  ///
+  /// - parameter pattern: A regular expression pattern to match against `self`.
+  /// - parameter template: A template string used to replace matches.
+  ///
+  /// - returns: A string with the first match of `pattern` replaced by `template`.
   public func replacingFirstMatching(pattern: String, with template: String) -> String {
     return replacingFirstMatching(Regex(pattern), with: template)
   }
 
   // MARK: Replacing all matches
 
+  /// Replace each substring matched by `regex` with `template`.
+  ///
+  /// The template string may be a literal string, or include template variables:
+  /// the variable `$0` will be replaced with the entire matched substring, `$1`
+  /// with the first capture group, etc.
+  ///
+  /// - note: To include the literal string "$1" in the replacement string,
+  ///   you must escape the "$": `\$1`.
+  ///
+  /// - parameter regex: A regular expression to match against `self`.
+  /// - parameter template: A template string used to replace matches.
   public mutating func replaceAllMatching(regex: Regex, with template: String) {
     for match in regex.allMatches(self).reverse() {
       let replacement = regex
@@ -44,16 +113,62 @@ extension String {
     }
   }
 
+  /// Replace each substring matched by the regular expression described in
+  /// `pattern` with `template`.
+  ///
+  /// The template string may be a literal string, or include template variables:
+  /// the variable `$0` will be replaced with the entire matched substring, `$1`
+  /// with the first capture group, etc.
+  ///
+  /// - note: To include the literal string "$1" in the replacement string,
+  ///   you must escape the "$": `\$1`.
+  ///
+  /// - requires: `pattern` is a valid regular expression. Invalid regular
+  ///   expressions will cause this method to trap.
+  ///
+  /// - parameter pattern: A regular expression pattern to match against `self`.
+  /// - parameter template: A template string used to replace matches.
   public mutating func replaceAllMatching(pattern: String, with template: String) {
     replaceAllMatching(Regex(pattern), with: template)
   }
 
+  /// Returns a new string where each substring matched by `regex` is replaced
+  /// with `template`.
+  ///
+  /// The template string may be a literal string, or include template variables:
+  /// the variable `$0` will be replaced with the entire matched substring, `$1`
+  /// with the first capture group, etc.
+  ///
+  /// - note: To include the literal string "$1" in the replacement string,
+  ///   you must escape the "$": `\$1`.
+  ///
+  /// - parameter regex: A regular expression to match against `self`.
+  /// - parameter template: A template string used to replace matches.
+  ///
+  /// - returns: A string with all matches of `regex` replaced by `template`.
   public func replacingAllMatching(regex: Regex, with template: String) -> String {
     var string = self
     string.replaceAllMatching(regex, with: template)
     return string
   }
 
+  /// Returns a new string where each substring matched by the regular
+  /// expression described in `pattern` is replaced with `template`.
+  ///
+  /// The template string may be a literal string, or include template variables:
+  /// the variable `$0` will be replaced with the entire matched substring, `$1`
+  /// with the first capture group, etc.
+  ///
+  /// - note: To include the literal string "$1" in the replacement string,
+  ///   you must escape the "$": `\$1`.
+  ///
+  /// - requires: `pattern` is a valid regular expression. Invalid regular
+  ///   expressions will cause this method to trap.
+  ///
+  /// - parameter pattern: A regular expression pattern to match against `self`.
+  /// - parameter template: A template string used to replace matches.
+  ///
+  /// - returns: A string with all matches of `pattern` replaced by `template`.
   public func replacingAllMatching(pattern: String, with template: String) -> String {
     return replacingAllMatching(Regex(pattern), with: template)
   }
