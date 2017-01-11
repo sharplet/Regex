31d50cf9aad8a6ee5922fd28366a6dc2ff8e6479
Adam Sharp <adam@sharplet.me>
2017-01-11 13:57:00 -0500

    Add Swift 3.0.2 / 3.1 (Linux) to CI build matrix
    Swift 3.1 builds are currently allowed to fail while Swift 3.1 is in
    development.

---
+++ bin/setup-swiftenv.bash
@@ -0,0 +1,14 @@
+# Adapted from: https://gist.githubusercontent.com/kylef/5c0475ff02b7c7671d2a/raw/9f442512a46d7a2af7b850d65a7e9bd31edfb09b/swiftenv-install.sh
+#
+# Automatically installs swiftenv and run's swiftenv install.
+# This script was designed for usage in CI systems.
+
+git clone --depth 1 https://github.com/kylef/swiftenv.git ~/.swiftenv
+export SWIFTENV_ROOT="$HOME/.swiftenv"
+export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"
+
+if [ -f ".swift-version" ] || [ -n "$SWIFT_VERSION" ]; then
+  swiftenv install -s
+else
+  swiftenv rehash
+fi
