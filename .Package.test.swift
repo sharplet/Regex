880f7dce6ad439a08bc312d6a219152717555fbb
Adam Sharp <adam@sharplet.me>
2017-06-12 17:41:11 -0400

    Add missing swiftLanguageVersions key in .Package.test.swift

--- .Package.test.swift
+++ .Package.test.swift
@@ -7,5 +7,6 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1),
     .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 7),
-  ]
+  ],
+  swiftLanguageVersions: [3, 4]
 )
