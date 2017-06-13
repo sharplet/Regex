4b1d873723b23e4f9f6bb8cce7ca3604041a16d9
Adam Sharp <adam@sharplet.me>
2017-06-13 10:52:40 -0400

    Drop support for swift < 3.1

--- Source/ThreadLocal.swift
+++ Source/ThreadLocal.swift
@@ -10,28 +10,10 @@ internal final class ThreadLocal<T> {
 
   var value: T? {
     get {
-#if swift(>=3.1)
       return Thread.current.threadDictionary[key] as? T
-#else
-      let box = Thread.current.threadDictionary[key] as? Box<T>
-      return box?.value
-#endif
     }
     set {
-#if !swift(>=3.1)
-      let newValue = newValue.map(Box.init(_:))
-#endif
       Thread.current.threadDictionary[key] = newValue
     }
   }
 }
-
-#if !swift(>=3.1)
-private final class Box<T> {
-  let value: T
-
-  init(_ value: T) {
-    self.value = value
-  }
-}
-#endif
