c3b392143fe81cc305fb5e185895b1b8baa14cba
Adam Sharp <adam@sharplet.me>
2017-06-13 11:43:22 -0400

    Add travis build for latest swift 4 snapshot

--- bin/setup-swiftenv.bash
+++ bin/setup-swiftenv.bash
@@ -6,9 +6,3 @@
 git clone --depth 1 https://github.com/kylef/swiftenv.git ~/.swiftenv
 export SWIFTENV_ROOT="$HOME/.swiftenv"
 export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"
-
-if [ -f ".swift-version" ] || [ -n "$SWIFT_VERSION" ]; then
-  swiftenv install -s
-else
-  swiftenv rehash
-fi
