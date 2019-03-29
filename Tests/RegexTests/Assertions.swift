2f947a43f2792da1b79648ebec7cbcf6353d7d65
Adam Sharp <adam@sharplet.me>
2019-03-29 17:24:37 -0400

    Move test assertions into Assertions.swift

---
+++ Tests/RegexTests/Assertions.swift
@@ -0,0 +1,26 @@
+import Regex
+import XCTest
+
+func XCTAssertMatches(_ regularExpression: Regex, _ stringMatch: String, _ file: StaticString = #file, line: UInt = #line) {
+  XCTAssertTrue(regularExpression.matches(stringMatch), "expected <\(regularExpression)> to match <\(stringMatch)>", file: file, line: line)
+}
+
+func XCTAssertDoesNotMatch(_ regularExpression: Regex, _ stringMatch: String, _ file: StaticString = #file, line: UInt = #line) {
+  XCTAssertFalse(regularExpression.matches(stringMatch), "expected <\(regularExpression)> not to match <\(stringMatch)>", file: file, line: line)
+}
+
+func XCTAssertCaptures(_ regularExpression: Regex, captures: String..., from string: String, _ file: StaticString = #file, line: UInt = #line) {
+  for expected in captures {
+    let match = regularExpression.firstMatch(in: string)
+    XCTAssertNotNil(match, file: file, line: line)
+    XCTAssertTrue(match!.captures.contains(where: { $0 == expected }), "expected <\(regularExpression)> to capture <\(captures)> from <\(string)> ", file: file, line: line)
+  }
+}
+
+func XCTAssertDoesNotCapture(_ regularExpression: Regex, captures: String..., from string: String, _ file: StaticString = #file, line: UInt = #line) {
+  for expected in captures {
+    let match = regularExpression.firstMatch(in: string)
+    XCTAssertNotNil(match, file: file, line: line)
+    XCTAssertFalse(match!.captures.contains(where: { $0 == expected }), "expected <\(regularExpression)> to not capture <\(captures)> from <\(string)> ", file: file, line: line)
+  }
+}
--- Tests/RegexTests/RegexTests.swift
+++ Tests/RegexTests/RegexTests.swift
@@ -151,27 +151,3 @@ final class RegexTests: XCTestCase {
     XCTAssertNil(Regex.lastMatch)
   }
 }
-
-private func XCTAssertMatches(_ regularExpression: Regex, _ stringMatch: String, _ file: StaticString = #file, line: UInt = #line) {
-  XCTAssertTrue(regularExpression.matches(stringMatch), "expected <\(regularExpression)> to match <\(stringMatch)>", file: file, line: line)
-}
-
-private func XCTAssertDoesNotMatch(_ regularExpression: Regex, _ stringMatch: String, _ file: StaticString = #file, line: UInt = #line) {
-  XCTAssertFalse(regularExpression.matches(stringMatch), "expected <\(regularExpression)> not to match <\(stringMatch)>", file: file, line: line)
-}
-
-private func XCTAssertCaptures(_ regularExpression: Regex, captures: String..., from string: String, _ file: StaticString = #file, line: UInt = #line) {
-  for expected in captures {
-    let match = regularExpression.firstMatch(in: string)
-    XCTAssertNotNil(match, file: file, line: line)
-    XCTAssertTrue(match!.captures.contains(where: { $0 == expected }), "expected <\(regularExpression)> to capture <\(captures)> from <\(string)> ", file: file, line: line)
-  }
-}
-
-private func XCTAssertDoesNotCapture(_ regularExpression: Regex, captures: String..., from string: String, _ file: StaticString = #file, line: UInt = #line) {
-  for expected in captures {
-    let match = regularExpression.firstMatch(in: string)
-    XCTAssertNotNil(match, file: file, line: line)
-    XCTAssertFalse(match!.captures.contains(where: { $0 == expected }), "expected <\(regularExpression)> to not capture <\(captures)> from <\(string)> ", file: file, line: line)
-  }
-}
